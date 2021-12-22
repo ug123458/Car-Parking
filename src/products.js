df09cd72fa647573b98f499bf8da3036600b6337
ug123458 <ug123458@gmail.com>
2021-12-22 10:56:24 +0530

    added discription

--- src/products.js
+++ src/products.js
@@ -4,7 +4,7 @@ const products = [
     name: 'DLF Mall of India',
     image: '/images/airpods.jpg',
     description:
-      'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+      'DLF Mall of India, a venture in Noida by one of the largest real estate companies in the country, DLF, offers you one of the largest shopping avenue divided into 7 floors and 5 unique shopping zones covering shopping, food and entertainment.',
     brand: 'Apple',
     category: 'Electronics',
     price: 1.99,
@@ -17,7 +17,7 @@ const products = [
     name: 'AeroCity',
     image: '/images/phone.jpg',
     description:
-      'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+      'Aerocity in Delhi is synonymous with the word luxury when it comes to the hotels, restaurants and even stores here! Built near the Indira Gandhi International Airport, the target audience is mostly travellers who have a layover in Delhi or happen to be in Delhi for a few hours.',
     brand: 'Apple',
     category: 'Electronics',
     price: 4.99,
@@ -30,7 +30,7 @@ const products = [
     name: 'Select City Walk',
     image: '/images/camera.jpg',
     description:
-      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+      'Select City Walk is located at Saket District Centre and is a pride of NCR. It houses some of the famous brands in Fashion, Apparel, Food, Foot Wear, Cosmetics, Entertainment, Gift Shops etc.',
     brand: 'Cannon',
     category: 'Electronics',
     price: 7.99,
@@ -43,7 +43,7 @@ const products = [
     name: 'Lodhiart District',
     image: '/images/playstation.jpg',
     description:
-      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+      'Lodhi Colony was the last housing estate build by the British, which bears a rich history in Delhis timeline for its iconic architecture. Since 2015, 50 renowned street artists from across the world have been invited by St+Art India Foundation to create the Lodhi Art District making it India.',
     brand: 'Sony',
     category: 'Electronics',
     price: 45.99,
@@ -56,7 +56,7 @@ const products = [
     name: 'Pacific Mall',
     image: '/images/mouse.jpg',
     description:
-      'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience',
+      'The vibrancy of East Delhi and nearby Ghaziabad area has zoomed up ever since the construction of Pacific Mall Ghaziabad, which is a major destination for shopping, entertainment, and carnival cinema.',
     brand: 'Logitech',
     category: 'Electronics',
     price: 78.99,
@@ -69,7 +69,7 @@ const products = [
     name: 'Connaught Place, New Delhi',
     image: '/images/alexa.jpg',
     description:
-      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+      'Connaught Place (also known as Rajiv Chowk) is one of the main financial, commercial and business centres in New Delhi, Delhi, India. Colloquially, and Connaught Place or CP (Rajiv Chowk or RC) are interchangeably used.',
     brand: 'Amazon',
     category: 'Electronics',
     price: 29.99,
